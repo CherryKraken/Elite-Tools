49f7b7cd56e0e7889c279f77597b8375b6d9217c
CherryKraken <connorboyle@sasktel.net>
2017-09-21 19:53:19 -0600

    Made hyperlinks clickable in main text field

--- app/src/main/java/com/connorboyle/elitetools/Notepad.java
+++ app/src/main/java/com/connorboyle/elitetools/Notepad.java
@@ -6,17 +6,32 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.Layout;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.TextWatcher;
+import android.text.method.ArrowKeyMovementMethod;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.util.Linkify;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.view.MotionEvent.ACTION_DOWN;
+import static android.view.MotionEvent.ACTION_UP;
 
 /**
  * Created by Connor Boyle on 2017-06-18.
  */
 
-public class Notepad extends AppCompatActivity {
+public class Notepad extends AppCompatActivity implements TextWatcher {
 
     EditText etTitle, etText;
     NoteEntry note;
@@ -32,9 +47,6 @@ public class Notepad extends AppCompatActivity {
 
         etTitle = (EditText) findViewById(R.id.etTitle);
         etText = (EditText) findViewById(R.id.etNotepad);
-//
-//        etText.setOnKeyListener(this);
-//        etTitle.setOnKeyListener(this);
 
         note = new NoteEntry("", "", System.currentTimeMillis());
 
@@ -43,15 +55,51 @@ public class Notepad extends AppCompatActivity {
             etText.setText(note.text);
             etTitle.setText(note.title);
         }
+
+        etText.setLinksClickable(true);
+        etText.setAutoLinkMask(Linkify.WEB_URLS);
+        etText.setMovementMethod(ExtendedLinkMovementMethod.getInstance());
+        Linkify.addLinks(etText, Linkify.WEB_URLS);
+        etText.addTextChangedListener(this);
     }
 
-//
-//    @Override
-//    public boolean onKey(View v, int keyCode, KeyEvent event) {
-//        note.title = etTitle.getText().toString();
-//        note.text = etText.getText().toString();
-//        return true;
-//    }
+    private static class ExtendedLinkMovementMethod extends ArrowKeyMovementMethod {
+        static ExtendedLinkMovementMethod sInstance;
+        public static ExtendedLinkMovementMethod getInstance() {
+            if (sInstance == null) {
+                sInstance = new ExtendedLinkMovementMethod();
+            }
+            return sInstance;
+        }
+
+        @Override
+        public boolean onTouchEvent(TextView widget, Spannable buffer, MotionEvent event) {
+            int action = event.getAction();
+            if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN) {
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                x -= widget.getTotalPaddingLeft();
+                y -= widget.getTotalPaddingTop();
+                x += widget.getScrollX();
+                y += widget.getScrollY();
+
+                Layout layout = widget.getLayout();
+                int line = layout.getLineForVertical(y);
+                int offset = layout.getOffsetForHorizontal(line, x);
+                ClickableSpan[] link = buffer.getSpans(offset, offset, ClickableSpan.class);
+
+                if (link.length != 0) {
+                    if (action == MotionEvent.ACTION_UP) {
+                        link[0].onClick(widget);
+                    } else if (action == MotionEvent.ACTION_DOWN) {
+                        Selection.setSelection(buffer, buffer.getSpanStart(link[0]), buffer.getSpanEnd(link[0]));
+                    }
+                }
+            }
+
+            return super.onTouchEvent(widget, buffer, event);
+        }
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -68,5 +116,14 @@ public class Notepad extends AppCompatActivity {
         }
         finish();
         return true;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) { }
+    @Override
+    public void afterTextChanged(Editable s) {
+        Linkify.addLinks(s, Linkify.WEB_URLS);
     }
 }
