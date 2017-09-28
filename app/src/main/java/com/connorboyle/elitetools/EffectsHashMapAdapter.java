14e3bc887cf9bb037ff607ff471142c56c996258
CherryKraken <connorboyle@sasktel.net>
2017-09-27 23:13:32 -0600

    Created abstract generic class HashMapAdapter and added an implementation for the Recipe effects hashmap

---
+++ app/src/main/java/com/connorboyle/elitetools/EffectsHashMapAdapter.java
@@ -0,0 +1,86 @@
+package com.connorboyle.elitetools;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+import static android.R.attr.max;
+
+/**
+ * Created by Connor Boyle on 27-Sep-17.
+ */
+
+class EffectsHashMapAdapter extends HashMapAdapter {
+    public EffectsHashMapAdapter(HashMap map) {
+        super(map);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View result = convertView != null ? convertView
+                : LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.effects_row_view, parent, false);
+
+        TextView tvEffect = (TextView) result.findViewById(R.id.tvRowEffectName);
+        TextView tvMin = (TextView) result.findViewById(R.id.tvRowEffectMin);
+        TextView tvMax = (TextView) result.findViewById(R.id.tvRowEffectMax);
+
+        double min = ((double[]) getValueAt(position))[0];
+        double max = ((double[]) getValueAt(position))[1];
+        String sMin, sMax, name = (String) getKeyAt(position);
+
+        // Check if values are exact integers (i.e. effect values are a range of percentages)
+        if (min == Math.floor(min) && max == Math.floor(max)) {
+            sMin = String.format("%d%%", (int) min);
+            sMax = String.format("%d%%", (int) max);
+        } else {
+            sMin = String.valueOf(min);
+            sMax = String.valueOf(max);
+        }
+
+        tvEffect.setText(name);
+        tvMin.setText(sMin);
+        tvMax.setText(sMax);
+
+        if (min < max) {
+            if (0 < max && 0 < min) {
+                tvMin.setTextColor(Color.CYAN);
+                tvMax.setTextColor(Color.CYAN);
+            } else if (min < 0 && max < 0) {
+                tvMin.setTextColor(Color.RED);
+                tvMax.setTextColor(Color.RED);
+            } else {
+                tvMin.setTextColor(Color.RED);
+                tvMax.setTextColor(Color.CYAN);
+            }
+        } else if (min > max) {
+            if (0 < max && 0 < min) {
+                tvMin.setTextColor(Color.RED);
+                tvMax.setTextColor(Color.RED);
+            } else if (min < 0 && max < 0) {
+                tvMin.setTextColor(Color.CYAN);
+                tvMax.setTextColor(Color.CYAN);
+            } else {
+                tvMin.setTextColor(Color.RED);
+                tvMax.setTextColor(Color.CYAN);
+            }
+        } else {
+            tvMin.setTextColor(Color.CYAN);
+            tvMax.setTextColor(Color.CYAN);
+        }
+
+        if (max == 0) {
+            tvMax.setTextColor(Color.parseColor("#ff8100"));
+        }
+        if (min == 0) {
+            tvMin.setTextColor(Color.parseColor("#ff8100"));
+        }
+
+        return result;
+    }
+}
---
+++ app/src/main/java/com/connorboyle/elitetools/HashMapAdapter.java
@@ -0,0 +1,43 @@
+package com.connorboyle.elitetools;
+
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Connor Boyle on 27-Sep-17.
+ */
+
+public abstract class HashMapAdapter<K, V> extends BaseAdapter {
+    private ArrayList<Map.Entry<K, V>> mEntries;
+
+    public HashMapAdapter(HashMap<K, V> map) {
+        mEntries = new ArrayList<>();
+        mEntries.addAll(map.entrySet());
+    }
+
+    @Override
+    public int getCount() {
+        return mEntries.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Map.Entry<K, V> getItem(int position) {
+        return mEntries.get(position);
+    }
+
+    public K getKeyAt(int position) {
+        return getItem(position).getKey();
+    }
+
+    public V getValueAt(int position) {
+        return getItem(position).getValue();
+    }
+}
