c4fd18d9a07293cd4dac61de7827f2aed36c5978
CherryKraken <connorboyle@sasktel.net>
2017-10-02 01:29:25 -0600

    Added Material (basic info) and extended RawMaterial (planetary bodies having raw element) classes

---
+++ app/src/main/java/com/connorboyle/elitetools/classes/Material.java
@@ -0,0 +1,39 @@
+package com.connorboyle.elitetools.classes;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Connor Boyle on 01-Oct-17.
+ */
+
+public class Material {
+    private static final String[] Grades = {
+            "Very Common",
+            "Common",
+            "Standard",
+            "Rare",
+            "Very Rare"
+    };
+
+    public String name;
+    private String grade;
+    private ArrayList<String> methods;
+
+    public Material(String name, String grade) {
+        this.name = name;
+        this.grade = grade;
+        methods = new ArrayList<>();
+    }
+
+    public void addMethod(String s) {
+        methods.add(s);
+    }
+
+    public ArrayList<String> getMethods() {
+        return methods;
+    }
+
+    public String getGrade() {
+        return String.format("%s (%s)", grade, Grades[Integer.valueOf(grade) - 1]);
+    }
+}
