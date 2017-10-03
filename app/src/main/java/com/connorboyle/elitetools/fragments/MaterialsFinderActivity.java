90acae32cd103cef25e9af881bbf6cbc388c0b21
CherryKraken <connorboyle@sasktel.net>
2017-10-03 00:51:18 -0600

    Fixed auto-complete not being populated

--- app/src/main/java/com/connorboyle/elitetools/fragments/MaterialsFinderActivity.java
+++ app/src/main/java/com/connorboyle/elitetools/fragments/MaterialsFinderActivity.java
@@ -6,10 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 import com.connorboyle.elitetools.R;
+import com.connorboyle.elitetools.asynctasks.GetIngredientsTask;
+import com.connorboyle.elitetools.classes.Material;
+
+import java.util.ArrayList;
 
 /**
  * Created by Connor Boyle on 20-Sep-17.
@@ -21,9 +27,12 @@ public class MaterialsFinderActivity extends Fragment {
     private TextView tvRarity;
     private AutoCompleteTextView etMatToFind, etCurSystem;
 
+    private ArrayList<String> materials;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.blueprint_layout, container, false);
+        v = inflater.inflate(R.layout.mat_finder_layout, container, false);
         setupControls();
+        new GetIngredientsTask(this).execute();
         return v;
     }
 
@@ -31,9 +40,38 @@ public class MaterialsFinderActivity extends Fragment {
         tvRarity = (TextView) v.findViewById(R.id.tvRarity);
         etMatToFind = (AutoCompleteTextView) v.findViewById(R.id.etMatToFind);
         etCurSystem = (AutoCompleteTextView) v.findViewById(R.id.etCurSystem);
+
+        tvRarity.setVisibility(View.INVISIBLE);
+        etCurSystem.setVisibility(View.GONE);
+
+        etMatToFind.setThreshold(2);
+        etCurSystem.setThreshold(2);
+
+        etMatToFind.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                onMaterialSelected(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                if (materials.contains(etMatToFind.getText().toString())) {
+                    onMaterialSelected(materials.indexOf(etMatToFind.getText().toString()));
+                }
+            }
+        });
     }
 
-    private double distBetween(double x1, double y1, double z1, double x2, double y2, double z2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2));
+    private void onMaterialSelected(int position) {
+        //new GetIngredientInfoTask(this).execute(materials.get(position));
+    }
+
+    public void onIngredientsTaskCompleted(ArrayList<String> strings) {
+        materials = strings;
+        etMatToFind.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, materials));
+    }
+
+    public void onMaterialInfoTaskCompleted(Material m) {
+
     }
 }
