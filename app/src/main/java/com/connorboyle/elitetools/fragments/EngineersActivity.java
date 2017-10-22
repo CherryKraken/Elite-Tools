29eba2f9ff3f2d7cff60b6e677bef4920587b85b
CherryKraken <connorboyle@sasktel.net>
2017-10-22 01:25:23 -0600

    Updated fragment to implement OnTaskCompleteHelper

--- app/src/main/java/com/connorboyle/elitetools/fragments/EngineersActivity.java
+++ app/src/main/java/com/connorboyle/elitetools/fragments/EngineersActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import com.connorboyle.elitetools.R;
 import com.connorboyle.elitetools.asynctasks.GetEngineerDetailTask;
 import com.connorboyle.elitetools.asynctasks.GetEngineersTask;
+import com.connorboyle.elitetools.asynctasks.OnTaskCompleteHelper;
 import com.connorboyle.elitetools.classes.Engineer;
 
 import java.util.ArrayList;
@@ -23,7 +24,7 @@ import java.util.ArrayList;
  * Created by Connor Boyle on 20-Sep-17.
  */
 
-public class EngineersActivity extends Fragment {
+public class EngineersActivity extends Fragment implements OnTaskCompleteHelper {
     private View v;
 
     private Spinner selEngineers;
@@ -41,35 +42,39 @@ public class EngineersActivity extends Fragment {
         selEngineers = (Spinner) v.findViewById(R.id.selEngineers);
     }
 
-    public void onEngineersTaskCompleted(ArrayList<String> strings) {
-        engrIDs = new ArrayList<>();
-        engrNames = new ArrayList<>();
-        int i = 0;
+    @Override
+    public void onAsyncTaskComplete(Task task, Object obj) {
+        switch (task) {
+            case ENGINEERS:engrIDs = new ArrayList<>();
+                ArrayList<String> strings = (ArrayList<String>) obj;
+                engrNames = new ArrayList<>();
+                int i = 0;
 
-        while (i < strings.size()) {
-            engrIDs.add(strings.get(i++));
-            engrNames.add(strings.get(i++));
+                while (i < strings.size()) {
+                    engrIDs.add(strings.get(i++));
+                    engrNames.add(strings.get(i++));
+                }
+
+                selEngineers.setAdapter(new ArrayAdapter<>(
+                        getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, engrNames));
+                selEngineers.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        new GetEngineerDetailTask(EngineersActivity.this).execute(engrIDs.get(position));
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) { }
+                });
+                break;
+            case ENGINEER_INFO:
+                Engineer engineer = (Engineer) obj;
+                ((TextView) v.findViewById(R.id.tvEngrName)).setText(engineer.name);
+                ((TextView) v.findViewById(R.id.tvEngrSystem)).setText(engineer.system);
+                ((TextView) v.findViewById(R.id.tvDReq)).setText(engineer.discover_req);
+                ((TextView) v.findViewById(R.id.tvMReq)).setText(engineer.meeting_req);
+                ((TextView) v.findViewById(R.id.tvUReq)).setText(engineer.unlock_req);
+                break;
         }
-
-        selEngineers.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, engrNames));
-        selEngineers.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                new GetEngineerDetailTask(EngineersActivity.this).execute(engrIDs.get(position));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
-
-    public void onEngineerObjectCreated(Engineer engineer) {
-        ((TextView) v.findViewById(R.id.tvEngrName)).setText(engineer.name);
-        ((TextView) v.findViewById(R.id.tvEngrSystem)).setText(engineer.system);
-        ((TextView) v.findViewById(R.id.tvDReq)).setText(engineer.discover_req);
-        ((TextView) v.findViewById(R.id.tvMReq)).setText(engineer.meeting_req);
-        ((TextView) v.findViewById(R.id.tvUReq)).setText(engineer.unlock_req);
     }
 }
