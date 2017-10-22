8f662521993fa47fe6e1a66591bb9e5061d25fb5
CherryKraken <connorboyle@sasktel.net>
2017-10-22 00:15:52 -0600

    Added classes to force a design for creating, executing, and completing AsyncTasks

---
+++ app/src/main/java/com/connorboyle/elitetools/asynctasks/OnTaskCompleteHelper.java
@@ -0,0 +1,28 @@
+package com.connorboyle.elitetools.asynctasks;
+
+/**
+ * Created by Connor Boyle on 21-Oct-17.
+ */
+
+public interface OnTaskCompleteHelper {
+    public enum Task {
+        SYSTEMS_LITE,
+        SYSTEMS_FULL,
+        SYSTEM_INFO,
+        RECIPE,
+        INGREDIENTS,
+        GRADES,
+        MODIFICATIONS,
+        MODULES,
+        ENGINEERS,
+        ENGINEERS_FOR_GRADE,
+        ENGINEER_INFO
+    }
+
+    /**
+     * Called when an AsyncTask has completed successfully
+     * @param task The task of task completed
+     * @param obj The data that was returned
+     */
+    public void onAsyncTaskComplete(Task task, Object obj);
+}
---
+++ app/src/main/java/com/connorboyle/elitetools/asynctasks/ParseJsonTask.java
@@ -0,0 +1,22 @@
+package com.connorboyle.elitetools.asynctasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Connor Boyle on 21-Oct-17.
+ */
+
+public abstract class ParseJsonTask<A, B, C> extends AsyncTask<A, B, C> {
+    protected final OnTaskCompleteHelper caller;
+    protected final OnTaskCompleteHelper.Task task;
+
+    protected ParseJsonTask(OnTaskCompleteHelper caller, OnTaskCompleteHelper.Task task) {
+        this.caller = caller;
+        this.task = task;
+    }
+
+    @Override
+    protected void onPostExecute(C c) {
+        caller.onAsyncTaskComplete(task, c);
+    }
+}
